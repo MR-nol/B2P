435022fe9588d942a0a07c3c7a2becf407ea1ed3
MR-nol <nol.lineas.infinitas@proton.me>
2026-07-28 09:37:42 -0600

    extendi el debbug

--- ayuda para debbugear.py
+++ ayuda para debbugear.py
@@ -1,6 +1,13 @@
-numerosEnterosDecimales = str([1,2,3,4,5,6,7,8,9,])
+numerosEnterosDecimales = [1,2,3,4,5,6,7,8,9,]
+
 for numerosEnterosDecimales in range(10):
     rutaSeleccionada = "/dev/sda1p4"
+    longitudRuta = len(rutaSeleccionada)
+    rutaSemidepurada = rutaSeleccionada[5:longitudRuta]
+    indiceParticion = rutaSemidepurada.find("p")
+    
 
-    indiceRuta = rutaSeleccionada.find(numerosEnterosDecimales)
-    print(indiceRuta)
+    indiceRuta = rutaSeleccionada.find(str(numerosEnterosDecimales))
+    #/dev = 3
+    print(rutaSemidepurada)
+print(indiceParticion)
